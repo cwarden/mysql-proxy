fab3012d7a090eb7f260457d27650963fd0f1d37
jan@mysql.com <>
2009-04-22 20:09:42 +0200

    added a simple lua interface to that calls a function for each event received

--- plugins/replicant/replicant-plugin.c
+++ plugins/replicant/replicant-plugin.c
@@ -43,6 +43,7 @@
 #include "network-mysqld.h"
 #include "network-mysqld-proto.h"
 #include "network-mysqld-binlog.h"
+#include "network-mysqld-binlog-lua.h"
 #include "network-mysqld-packet.h"
 #include "sys-pedantic.h"
 #include "glib-ext.h"
@@ -87,8 +88,7 @@ struct chassis_plugin_config {
 
 	gchar *mysqld_username;
 	gchar *mysqld_password;
-
-	gchar **read_binlogs;
+	gchar *lua_script;
 
 	network_mysqld_con *listen_con;
 };
@@ -116,16 +116,17 @@ static void plugin_con_state_free(plugin_con_state *st) {
 static int network_mysqld_resultset_master_status(chassis *UNUSED_PARAM(chas), network_mysqld_con *con) {
 	GList *chunk;
 	guint i;
-	network_socket *sock = con->client;
+	network_socket *sock = con->server;
 	plugin_con_state *st = con->plugin_con_state;
 	GPtrArray *fields;
 	int err = 0;
 
 	/* scan the resultset */
-	chunk = sock->send_queue->chunks->head;
+	chunk = sock->recv_queue->chunks->head;
 
 	fields = network_mysqld_proto_fielddefs_new();
 	chunk = network_mysqld_proto_get_fielddefs(chunk, fields);
+	g_assert(chunk);
 
 	/* a data row */
 	while (NULL != (chunk = chunk->next)) {
@@ -135,7 +136,8 @@ static int network_mysqld_resultset_master_status(chassis *UNUSED_PARAM(chas), n
 		packet.data = chunk->data;
 		packet.offset = 0;
 
-		err = err || network_mysqld_proto_get_int8(&packet, &status);
+		err = err || network_mysqld_proto_skip_network_header(&packet);
+		err = err || network_mysqld_proto_peek_int8(&packet, &status);
 
 		/* if we find the 2nd EOF packet we are done */
 		if (status == MYSQLD_PACKET_EOF &&
@@ -188,7 +190,6 @@ NETWORK_MYSQLD_PLUGIN_PROTO(repclient_read_handshake) {
 	send_sock = con->server;
 
 	/* there should only be on packet */
-
 	packet.data = g_queue_peek_tail(recv_sock->recv_queue->chunks);
 	packet.offset = 0;
 
@@ -200,6 +201,7 @@ NETWORK_MYSQLD_PLUGIN_PROTO(repclient_read_handshake) {
 		 * client
 		 */
 
+			g_critical("%s", G_STRLOC);
 		return NETWORK_SOCKET_ERROR;
 	}
 
@@ -223,8 +225,9 @@ NETWORK_MYSQLD_PLUGIN_PROTO(repclient_read_handshake) {
 
 	auth = network_mysqld_auth_response_new();
 
-	auth->capabilities = shake->capabilities;
-	auth->charset      = shake->charset;
+	auth->capabilities    = CLIENT_BASIC_FLAGS;
+	auth->charset         = shake->charset;
+	auth->max_packet_size = 1 * 1024 * 1024;
 
 	if (config->mysqld_username) {
 		g_string_append(auth->username, config->mysqld_username);
@@ -237,6 +240,7 @@ NETWORK_MYSQLD_PLUGIN_PROTO(repclient_read_handshake) {
 	network_mysqld_proto_append_auth_response(auth_packet, auth);
 
 	network_mysqld_queue_append(send_sock->send_queue, S(auth_packet), send_sock->packet_id + 1);
+
 
 	network_mysqld_auth_response_free(auth);
 	network_mysqld_auth_challenge_free(shake);
@@ -259,6 +263,7 @@ NETWORK_MYSQLD_PLUGIN_PROTO(repclient_read_auth_result) {
 		;
 
 	recv_sock = con->server;
+	send_sock = con->server;
 
 	packet.data = g_queue_peek_tail(recv_sock->recv_queue->chunks);
 	packet.offset = 0;
@@ -286,7 +291,7 @@ NETWORK_MYSQLD_PLUGIN_PROTO(repclient_read_auth_result) {
 					G_STRLOC,
 					err_packet->errmsg->len ? err_packet->errmsg->str : "",
 					err_packet->errcode);
-		}
+		} 
 
 		network_mysqld_err_packet_free(err_packet);
 
@@ -305,7 +310,6 @@ NETWORK_MYSQLD_PLUGIN_PROTO(repclient_read_auth_result) {
 
 	g_string_free(g_queue_pop_tail(recv_sock->recv_queue->chunks), TRUE);
 
-	send_sock = con->server;
 	network_mysqld_queue_append(send_sock->send_queue, C(query_packet), 0);
 
 	con->state = CON_STATE_SEND_QUERY;
@@ -339,37 +343,40 @@ NETWORK_MYSQLD_PLUGIN_PROTO(repclient_read_query_result) {
 	 */
 	network_packet packet;
 	GList *chunk;
-	network_socket *recv_sock, *send_sock;
+	network_socket *recv_sock;
 	int is_finished = 0;
 	plugin_con_state *st = con->plugin_con_state;
+	chassis_plugin_config *config = con->config;
 	guint8 status;
 	int err = 0;
+	GString *query_packet;
+	int my_server_id = 2;
+	network_mysqld_binlog_dump *dump;
 
 	recv_sock = con->server;
-	send_sock = con->client;
 
 	chunk = recv_sock->recv_queue->chunks->tail;
 	packet.data = chunk->data;
 	packet.offset = 0;
-
+	
 	if (packet.data->len != recv_sock->packet_len + NET_HEADER_SIZE) return NETWORK_SOCKET_SUCCESS; /* packet isn't finished yet */
-#if 0
-	g_message("%s.%d: packet-len: %08x, packet-id: %d, command: COM_(%02x)", 
-			__FILE__, __LINE__,
-			recv_sock->packet_len,
-			recv_sock->packet_id,
-			con->parse.command
-		);
-#endif						
 
 	is_finished = network_mysqld_proto_get_query_result(&packet, con);
-	if (is_finished == -1) return NETWORK_SOCKET_ERROR;
+	if (is_finished == -1) {
+		g_message("%s", G_STRLOC);
+		return NETWORK_SOCKET_ERROR;
+	}
+	recv_sock->packet_len = PACKET_LEN_UNSET;
+	con->resultset_is_finished = is_finished;
+	if (is_finished == 0) return NETWORK_SOCKET_SUCCESS; /* wait for more */
+
+	packet.data = chunk->data;
+	packet.offset = 0;
+	err = err || network_mysqld_proto_skip_network_header(&packet);
+	err = err || network_mysqld_proto_peek_int8(&packet, &status);
 
 	switch (con->parse.command) {
 	case COM_BINLOG_DUMP:
-		packet.offset = 0;
-
-		err = err || network_mysqld_proto_skip_network_header(&packet);
 		err = err || network_mysqld_proto_get_int8(&packet, &status);
 
 		switch (status) {
@@ -381,80 +388,94 @@ NETWORK_MYSQLD_PLUGIN_PROTO(repclient_read_query_result) {
 			binlog = network_mysqld_binlog_new();
 			event = network_mysqld_binlog_event_new();
 
-			network_mysqld_proto_skip_network_header(&packet);
-			network_mysqld_proto_get_binlog_status(&packet);
-			network_mysqld_proto_get_binlog_event_header(&packet, event);
-			network_mysqld_proto_get_binlog_event(&packet, binlog, event);
+			err = err || network_mysqld_proto_get_binlog_event_header(&packet, event);
+			err = err || network_mysqld_proto_get_binlog_event(&packet, binlog, event);
 
-			/* do something */
+			if (!err && config->lua_script) {
+				lua_State *L;
+				/* call lua to expose the event */
+
+				L = luaL_newstate();
+
+				luaL_openlibs(L);
+
+				if (0 != luaL_loadfile(L, config->lua_script)) {
+					g_critical("%s: %s", G_STRLOC, lua_tostring(L, -1));
+					return NETWORK_SOCKET_ERROR;
+				}
+				if (0 != lua_pcall(L, 0, 0, 0)) {
+					g_critical("%s: %s", G_STRLOC, lua_tostring(L, -1));
+					return NETWORK_SOCKET_ERROR;
+				}
+				lua_getglobal(L, "binlog_event_iterate");
+				lua_mysqld_binlog_event_push(L, event, FALSE);
+				if (0 != lua_pcall(L, 1, 1, 0)) {
+					g_critical("%s: %s", G_STRLOC, lua_tostring(L, -1));
+					return NETWORK_SOCKET_ERROR;
+				}
+
+				lua_close(L);
+			}
 
 			network_mysqld_binlog_event_free(event);
 			network_mysqld_binlog_free(binlog);
+			
+			con->state = CON_STATE_READ_QUERY_RESULT;
 
 			break; }
 		default:
+			con->state = CON_STATE_ERROR;
 			break;
 		}
+		break;
+	case COM_QUERY:
 		break;
 	default:
 		break;
 	}
 
-	network_mysqld_queue_append(send_sock->send_queue, 
-			packet.data->str + NET_HEADER_SIZE, 
-			packet.data->len - NET_HEADER_SIZE, 
-			recv_sock->packet_id);
+	/**
+	 * the resultset handler might decide to trash the send-queue
+	 * 
+	 * */
 
-	/* ... */
-	if (is_finished) {
-		/**
-		 * the resultset handler might decide to trash the send-queue
-		 * 
-		 * */
-		GString *query_packet;
-		int my_server_id = 2;
-		network_mysqld_binlog_dump *dump;
-		GString *s_packet;
+	switch (st->state) {
+	case REPCLIENT_BINLOG_GET_POS:
+		/* parse the result-set and get the 1st and 2nd column */
 
-		switch (st->state) {
-		case REPCLIENT_BINLOG_GET_POS:
-			/* parse the result-set and get the 1st and 2nd column */
-
-			network_mysqld_resultset_master_status(chas, con);
-
-			/* remove all packets */
-			while ((s_packet = g_queue_pop_head(send_sock->send_queue->chunks))) g_string_free(s_packet, TRUE);
-
-			st->state = REPCLIENT_BINLOG_DUMP;
-
-			dump = network_mysqld_binlog_dump_new();
-			dump->binlog_pos  = st->binlog_pos;
-			dump->server_id   = my_server_id;
-			dump->binlog_file = g_strdup(st->binlog_file);
-
-			query_packet = g_string_new(NULL);
-
-			network_mysqld_proto_append_binlog_dump(query_packet, dump);
-		       	
-			send_sock = con->server;
-			network_mysqld_queue_append(send_sock->send_queue, query_packet->str, query_packet->len, 0);
-
-			network_mysqld_binlog_dump_free(dump);
-		
-			g_string_free(query_packet, TRUE);
-		
-			con->state = CON_STATE_SEND_QUERY;
-
-			break;
-		case REPCLIENT_BINLOG_DUMP:
-			/* remove all packets */
-
-			/* trash the packets for the injection query */
-			while ((s_packet = g_queue_pop_head(send_sock->send_queue->chunks))) g_string_free(s_packet, TRUE);
-
-			con->state = CON_STATE_READ_QUERY_RESULT;
-			break;
+		err = err || network_mysqld_resultset_master_status(chas, con);
+		if (err) {
+			g_message("%s", G_STRLOC);
+			return NETWORK_SOCKET_ERROR;
 		}
+
+		st->state = REPCLIENT_BINLOG_DUMP;
+
+		dump = network_mysqld_binlog_dump_new();
+		dump->binlog_pos  = st->binlog_pos;
+		dump->server_id   = my_server_id;
+		dump->binlog_file = g_strdup(st->binlog_file);
+
+		query_packet = g_string_new(NULL);
+
+		network_mysqld_proto_append_binlog_dump(query_packet, dump);
+	       	
+		network_mysqld_queue_append(con->server->send_queue, S(query_packet), 0);
+
+		network_mysqld_binlog_dump_free(dump);
+	
+		g_string_free(query_packet, TRUE);
+
+		con->state = CON_STATE_SEND_QUERY;
+		network_mysqld_con_reset_command_response_state(con);
+
+		break;
+	case REPCLIENT_BINLOG_DUMP:
+		/* remove all packets */
+
+		/* trash the packets for the injection query */
+
+		break;
 	}
 
 	if (chunk->data) g_string_free(chunk->data, TRUE);
@@ -469,21 +490,42 @@ NETWORK_MYSQLD_PLUGIN_PROTO(repclient_connect_server) {
 	chassis_plugin_config *config = con->config;
 	gchar *address = config->master_address;
 
+	/* the 2nd round */
+	if (con->server) {
+		switch (network_socket_connect_finish(con->server)) {
+		case NETWORK_SOCKET_SUCCESS:
+			con->state = CON_STATE_READ_HANDSHAKE;
+			return NETWORK_SOCKET_SUCCESS;
+		case NETWORK_SOCKET_ERROR:
+		case NETWORK_SOCKET_ERROR_RETRY:
+		default:
+			g_critical("%s: %s", G_STRLOC, g_strerror(errno));
+			return NETWORK_SOCKET_ERROR;
+		}
+	}
 	con->server = network_socket_new();
 
 	if (0 != network_address_set_address(con->server->dst, address)) {
-		return -1;
+		g_critical("%s: network_address_set_address() failed", G_STRLOC);
+		return NETWORK_SOCKET_ERROR;
 	}
     
 	/* FIXME ... add non-blocking support (getsockopt()) */
 
-	if (0 != network_socket_connect(con->server)) {
-		return -1;
+	switch (network_socket_connect(con->server)) {
+	case NETWORK_SOCKET_ERROR:
+		g_critical("%s: network_socket_connect() failed", G_STRLOC);
+		return NETWORK_SOCKET_ERROR;
+	case NETWORK_SOCKET_SUCCESS:
+		con->state = CON_STATE_READ_HANDSHAKE;
+		return NETWORK_SOCKET_SUCCESS;
+	case NETWORK_SOCKET_ERROR_RETRY:
+		/* ask it to come back again */
+		return NETWORK_SOCKET_ERROR_RETRY;
+	default:
+		g_message("%s", G_STRLOC);
+		return NETWORK_SOCKET_ERROR;
 	}
-
-	con->state = CON_STATE_SEND_HANDSHAKE;
-
-	return NETWORK_SOCKET_SUCCESS;
 }
 
 NETWORK_MYSQLD_PLUGIN_PROTO(repclient_init) {
@@ -543,7 +585,6 @@ void network_mysqld_replicant_plugin_free(chassis_plugin_config *config) {
 
 	if (config->mysqld_username) g_free(config->mysqld_username);
 	if (config->mysqld_password) g_free(config->mysqld_password);
-	if (config->read_binlogs) g_strfreev(config->read_binlogs);
 
 	g_free(config);
 }
@@ -560,7 +601,7 @@ static GOptionEntry * network_mysqld_replicant_plugin_get_options(chassis_plugin
 		{ "replicant-master-address",            0, 0, G_OPTION_ARG_STRING, NULL, "... (default: :4040)", "<host:port>" },
 		{ "replicant-username",                  0, 0, G_OPTION_ARG_STRING, NULL, "username", "" },
 		{ "replicant-password",                  0, 0, G_OPTION_ARG_STRING, NULL, "password", "" },
-		{ "replicant-read-binlogs",              0, 0, G_OPTION_ARG_FILENAME_ARRAY, NULL, "binlog files", "" },
+		{ "replicant-lua-script",                0, 0, G_OPTION_ARG_STRING, NULL, "filename", "" },
 		{ NULL,                       0, 0, G_OPTION_ARG_NONE,   NULL, NULL, NULL }
 	};
 
@@ -568,257 +609,31 @@ static GOptionEntry * network_mysqld_replicant_plugin_get_options(chassis_plugin
 	config_entries[i++].arg_data = &(config->master_address);
 	config_entries[i++].arg_data = &(config->mysqld_username);
 	config_entries[i++].arg_data = &(config->mysqld_password);
-	config_entries[i++].arg_data = &(config->read_binlogs);
+	config_entries[i++].arg_data = &(config->lua_script);
 	
 	return config_entries;
 }
 
-int network_mysqld_binlog_event_print(network_mysqld_binlog_event *event) {
-	guint i;
-	int metadata_offset = 0;
-#if 1
-	g_message("%s: timestamp = %u, type = %u, server-id = %u, size = %u, pos = %u, flags = %04x",
-			G_STRLOC,
-			event->timestamp,
-			event->event_type,
-			event->server_id,
-			event->event_size,
-			event->log_pos,
-			event->flags);
-#endif
-
-	switch (event->event_type) {
-	case QUERY_EVENT: /* 2 */
-#if 1
-		g_message("%s: QUERY: thread_id = %d, exec_time = %d, error-code = %d\ndb = %s, query = %s",
-				G_STRLOC,
-				event->event.query_event.thread_id,
-				event->event.query_event.exec_time,
-				event->event.query_event.error_code,
-				event->event.query_event.db_name ? event->event.query_event.db_name : "(null)",
-				event->event.query_event.query ? event->event.query_event.query : "(null)"
-			 );
-#endif
-		break;
-	case STOP_EVENT:
-		break;
-	case TABLE_MAP_EVENT:
-		g_message("%s: (table-definition) table-id = %"G_GUINT64_FORMAT", flags = %04x, db = %s, table = %s",
-				G_STRLOC,
-				event->event.table_map_event.table_id,
-				event->event.table_map_event.flags,
-				event->event.table_map_event.db_name ? event->event.table_map_event.db_name : "(null)",
-				event->event.table_map_event.table_name ? event->event.table_map_event.table_name : "(null)"
-			 );
-		g_message("%s: (table-definition) columns = %"G_GUINT64_FORMAT,
-				G_STRLOC,
-				event->event.table_map_event.columns_len
-			 );
-
-		/* the metadata is field specific */
-		for (i = 0; i < event->event.table_map_event.columns_len; i++) {
-			MYSQL_FIELD *field = network_mysqld_proto_fielddef_new();
-			enum enum_field_types col_type;
-
-			col_type = event->event.table_map_event.columns[i];
-
-			/* the meta-data depends on the type,
-			 *
-			 * string has 2 byte field-length
-			 * floats have precision
-			 * ints have display length
-			 * */
-			switch (col_type) {
-			case MYSQL_TYPE_STRING: /* 254 */
-				/* byte 0: real_type 
-				 * byte 1: field-length
-				 */
-				field->type = event->event.table_map_event.metadata[metadata_offset + 0];
-				field->length = event->event.table_map_event.metadata[metadata_offset + 1];
-				metadata_offset += 2;
-				break;
-			case MYSQL_TYPE_VAR_STRING:
-				/* 2 byte length (int2store)
-				 */
-				field->type = col_type;
-				field->length = 
-					((guchar)event->event.table_map_event.metadata[metadata_offset + 0]) |
-					((guchar)event->event.table_map_event.metadata[metadata_offset + 1]) << 8;
-				metadata_offset += 2;
-				break;
-			case MYSQL_TYPE_BLOB: /* 252 */
-				field->type = col_type;
-				metadata_offset += 1; /* the packlength (1 .. 4) */
-				break;
-			case MYSQL_TYPE_DECIMAL:
-				field->type = col_type;
-				metadata_offset += 2;
-				/**
-				 * byte 0: precisions
-				 * byte 1: decimals
-				 */
-				break;
-			case MYSQL_TYPE_DOUBLE:
-			case MYSQL_TYPE_FLOAT:
-				field->type = col_type;
-				/* pack-length */
-				metadata_offset += 1;
-				break;
-			case MYSQL_TYPE_ENUM:
-				/* real-type (ENUM|SET)
-				 * pack-length
-				 */
-				field->type = event->event.table_map_event.metadata[metadata_offset + 0];
-				metadata_offset += 2;
-				break;
-			case MYSQL_TYPE_BIT:
-				metadata_offset += 2;
-				break;
-			default:
-				field->type = col_type;
-				metadata_offset += 0;
-				break;
-			}
-
-			g_message("%s: (column-definition) [%d] type = %d, length = %lu",
-					G_STRLOC,
-					i,
-					field->type,
-					field->length
-				 );
-
-			network_mysqld_proto_fielddef_free(field);
-		}
-		break;
-	case FORMAT_DESCRIPTION_EVENT: /* 15 */
-		break;
-	case INTVAR_EVENT: /* 5 */
-	 	break;
-	case XID_EVENT: /* 16 */
-		break;
-	case ROTATE_EVENT: /* 4 */
-		break;
-	default:
-		g_message("%s: unknown event-type: %d",
-				G_STRLOC,
-				event->event_type);
-		return -1;
-	}
-	return 0;
-}
-
-int replicate_binlog_dump_file(const char *filename) {
-	int fd;
-	char binlog_header[4];
-	network_packet *packet;
-	network_mysqld_binlog *binlog;
-	network_mysqld_binlog_event *event;
-
-	if (-1 == (fd = g_open(filename, O_RDONLY, 0))) {
-		g_critical("%s: opening '%s' failed: %s",
-				G_STRLOC,
-				filename,
-				g_strerror(errno));
-		return -1;
-	}
-
-	if (4 != read(fd, binlog_header, 4)) {
-		g_return_val_if_reached(-1);
-	}
-
-	if (binlog_header[0] != '\xfe' ||
-	    binlog_header[1] != 'b' ||
-	    binlog_header[2] != 'i' ||
-	    binlog_header[3] != 'n') {
-
-		g_critical("%s: binlog-header should be: %02x%02x%02x%02x, got %02x%02x%02x%02x",
-				G_STRLOC,
-				'\xfe', 'b', 'i', 'n',
-				binlog_header[0],
-				binlog_header[1],
-				binlog_header[2],
-				binlog_header[3]
-				);
-
-		g_return_val_if_reached(-1);
-	}
-
-	packet = network_packet_new();
-	packet->data = g_string_new(NULL);
-	g_string_set_size(packet->data, 19 + 1);
-
-	binlog = network_mysqld_binlog_new();
-
-	/* next are the events, without the mysql packet header */
-	while (19 == (packet->data->len = read(fd, packet->data->str, 19))) {
-		gssize len;
-		packet->data->str[packet->data->len] = '\0'; /* term the string */
-
-		g_assert_cmpint(packet->data->len, ==, 19);
-
-		event = network_mysqld_binlog_event_new();
-		network_mysqld_proto_get_binlog_event_header(packet, event);
-
-		g_assert_cmpint(event->event_size, >=, 19);
-
-		g_string_set_size(packet->data, event->event_size); /* resize the string */
-		packet->data->len = 19;
-
-		len = read(fd, packet->data->str + 19, event->event_size - 19);
-
-		if (-1 == len) {
-			g_critical("%s: lseek(..., %d, ...) failed: %s",
-					G_STRLOC,
-					event->event_size - 19,
-					g_strerror(errno));
-			return -1;
-		}
-		g_assert_cmpint(len, ==, event->event_size - 19);
-		g_assert_cmpint(packet->data->len, ==, 19);
-		packet->data->len += len;
-		g_assert_cmpint(packet->data->len, ==, event->event_size);
-		
-		if (network_mysqld_proto_get_binlog_event(packet, binlog, event)) {
-			g_debug_hexdump(G_STRLOC, packet->data->str + 19, packet->data->len - 19);
-		} else if (network_mysqld_binlog_event_print(event)) {
-			/* ignore it */
-		}
-	
-		network_mysqld_binlog_event_free(event);
-
-		packet->offset = 0;
-	}
-	g_string_free(packet->data, TRUE);
-	network_packet_free(packet);
-
-	network_mysqld_binlog_free(binlog);
-
-	close(fd);
-
-	return 0;
-}
 
 /**
  * init the plugin with the parsed config
  */
 int network_mysqld_replicant_plugin_apply_config(chassis G_GNUC_UNUSED *chas, chassis_plugin_config *config) {
+	network_mysqld_con *con;
+
 	if (!config->master_address) config->master_address = g_strdup(":4040");
 	if (!config->mysqld_username) config->mysqld_username = g_strdup("repl");
 	if (!config->mysqld_password) config->mysqld_password = g_strdup("");
+	
+	g_message("%s: setting defaults for the replicant", G_STRLOC);
 
-	if (config->read_binlogs) {
-		int i;
+	/* setup the connection and */
+	con = network_mysqld_con_new();
+	con->srv = chas;
+	con->config = config;
+	network_mysqld_repclient_connection_init(chas, con);
 
-		/* we have a list of filenames we shall decode */
-		for (i = 0; config->read_binlogs[i]; i++) {
-			char *filename = config->read_binlogs[i];
-
-			replicate_binlog_dump_file(filename);
-		}
-
-		/* we are done, shutdown */
-		chassis_set_shutdown();
-	}
+	network_mysqld_con_handle(-1, 0, con);
 
 	return 0;
 }
