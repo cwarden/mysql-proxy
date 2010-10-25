82b06f00393041dec11b1c52a4c6e27db6697352
jan@mysql.com <>
2010-10-25 17:43:03 +0200

    be picky about missing references

--- doc/conf.py
+++ doc/conf.py
@@ -86,6 +86,8 @@ pygments_style = 'sphinx'
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
 
+nitpicky=True
+
 
 # -- Options for HTML output ---------------------------------------------------
 
